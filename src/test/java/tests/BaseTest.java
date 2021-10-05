93da1b3f22da6cdf83672da01b7eb9b79a700498
tito <relaxingtito@gmail.com>
2021-10-05 18:30:15 +0300

    add remote configuration to use selenoid

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -8,6 +8,7 @@ public class BaseTest {
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.baseUrl = "https://demoqa.com";
     }
 
